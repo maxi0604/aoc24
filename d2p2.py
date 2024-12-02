e808e283ed5eca28ac68ae79ba891d1c5191ca3f
mxi <public@complexvector.space>
2024-12-02 21:52:24 +0100

    goddammit this code sucks

--- d2p2.py
+++ d2p2.py
@@ -1,43 +1,41 @@
 import sys
+count = 0
 
-
-
-
-def check_asc(nums):
-    wrong = 0
+def check(nums, k):
+    desc, asc = True, True
     skip = False
     for i in range(1, len(nums)):
-        if skip:
-            skip = False
+        prev = nums[i - 1]
+        if i == k or (i == 1 and k == 0):
             continue
-        diff = nums[i] - nums[i - 1]
-        if not 1 <= diff <= 3:
-            wrong += 1
+        elif i == k + 1:
+            prev = nums[i - 2]
 
-    return wrong <= 1
+        if prev > nums[i]:
+            asc = False
+        elif prev < nums[i]:
+            desc = False
 
+        dist = abs(nums[i] - prev)
 
-def check_desc(nums):
-    wrong = 0
-    skip = False
-    for i in range(1, len(nums)):
-        if skip:
-            skip = False
-            continue
-        diff = nums[i] - nums[i - 1]
-        if not -3 <= diff <= -1:
-            wrong += 1
+        if dist < 1 or dist > 3:
+            skip = True
+            break
 
-    return wrong <= 1
+    if (desc or asc) and not skip:
+        return True
+    return False
 
-
-
-n = 0
-for i, line in enumerate(sys.stdin):
+for line in sys.stdin:
     nums = [int(a) for a in line.split()]
-    if check_asc(nums) or check_desc(nums):
-        n += 1
-        # print(i)
+    desc, asc = True, True
+    skip = False
+    for k in range(-1, len(nums)):
+        if check(nums, k):
+            count += 1
+            break
 
-print(n)
+
+
+print(count)
 
