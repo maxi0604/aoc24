66f7ceb49b6326e5ceb3340f0228fe2968c1ca4b
mxi <public@complexvector.space>
2024-12-02 21:45:20 +0100

    mental crisis

--- d2p2.py
+++ d2p2.py
@@ -1,20 +1,43 @@
 import sys
-count = 0
 
-for line in sys.stdin:
-    nums = [int(a) for a in line.split()]
-    desc, asc, wrong = 0, 0, 0
-    removed = False
+
+
+
+def check_asc(nums):
+    wrong = 0
     skip = False
     for i in range(1, len(nums)):
-        dist = nums[i] - nums[i - 1]
-
-        if dist < 1 or dist > 3:
+        if skip:
+            skip = False
+            continue
+        diff = nums[i] - nums[i - 1]
+        if not 1 <= diff <= 3:
             wrong += 1
 
-    if (desc + wrong <= 1) or (asc + wrong <= 1):
-        count += 1
+    return wrong <= 1
 
 
-print(count)
+def check_desc(nums):
+    wrong = 0
+    skip = False
+    for i in range(1, len(nums)):
+        if skip:
+            skip = False
+            continue
+        diff = nums[i] - nums[i - 1]
+        if not -3 <= diff <= -1:
+            wrong += 1
+
+    return wrong <= 1
+
+
+
+n = 0
+for i, line in enumerate(sys.stdin):
+    nums = [int(a) for a in line.split()]
+    if check_asc(nums) or check_desc(nums):
+        n += 1
+        # print(i)
+
+print(n)
 
